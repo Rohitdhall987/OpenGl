ec4dce11de09b8033737ab0c726f5d22759eb7ca
Rohitdhall987 <rohitdhall987@gmail.com>
2025-10-24 20:00:49 +0530

    window created

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,25 @@
-
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main(void)
 {
+    glfwInit();
+
+    GLFWwindow* window = glfwCreateWindow(800,600,"LEARN OPENGL",NULL,NULL);
+
+    glfwMakeContextCurrent(window);
+
+    glewInit();
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glClearColor(0.3f,0.3f,0.7f,1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
